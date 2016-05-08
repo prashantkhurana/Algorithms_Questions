183b60baadd9cb157c9b736337f203e8fcbb929b
Prashant Khurana <prashant.khurana@tutanota.com>
2016-05-08 17:05:43 -0400

    will it stop

---
+++ WILLITST.java
@@ -0,0 +1,59 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        WILLITST solver = new WILLITST();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class WILLITST {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            long n = Long.parseLong(in.next());
+            if (n <= 1 || ((n & (n - 1)) == 0)) {
+                out.println("TAK");
+            } else {
+                out.println("NIE");
+            }
+        }
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (Exception e) {
+                    return "";
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+    }
+}
+
