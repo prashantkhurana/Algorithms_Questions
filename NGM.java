1b26c7fd7721375cc870743a82d98fb53984d83e
Prashant Khurana <prashant.khurana@tutanota.com>
2016-05-31 20:33:58 -0400

    NGM

---
+++ NGM.java
@@ -0,0 +1,60 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        NGM solver = new NGM();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class NGM {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            String input = in.next();
+            if (input.charAt(input.length() - 1) == '0') {
+                out.println("2");
+            } else {
+                out.println("1");
+                out.println(input.charAt(input.length() - 1));
+            }
+        }
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (Exception e) {
+                    return "";
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+    }
+}
+
