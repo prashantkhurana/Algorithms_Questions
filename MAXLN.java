f53fcf44c78c801d83029754a0a2ee1104ffc29e
Prashant Khurana <prashant.khurana@tutanota.com>
2016-07-09 18:37:29 -0400

    MAXLN

---
+++ MAXLN.java
@@ -0,0 +1,64 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        MAXLN solver = new MAXLN();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class MAXLN {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            int testCases = in.nextInt();
+            for (int i = 0; i < testCases; i++) {
+                BigDecimal r = new BigDecimal(in.next());
+                BigDecimal ans = r.multiply(r).multiply(new BigDecimal(4)).add(new BigDecimal(0.25d)).setScale(2, BigDecimal.ROUND_HALF_UP);
+                out.println("Case " + (i + 1) + ": " + ans);
+            }
+        }
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (Exception e) {
+                    return "";
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
