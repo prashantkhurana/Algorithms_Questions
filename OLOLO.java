1a12850c08d822e4360e4af4b5331b529ed85eb0
Prashant Khurana <prashant.khurana@tutanota.com>
2016-07-09 18:19:15 -0400

    ollo tle

---
+++ OLOLO.java
@@ -0,0 +1,63 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        OLOLO solver = new OLOLO();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class OLOLO {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            int ans = 0;
+            int n = in.nextInt();
+            for (int i = 0; i < n; i++) {
+                ans = ans ^ in.nextInt();
+            }
+            out.print(ans);
+        }
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (Exception e) {
+                    return "";
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
